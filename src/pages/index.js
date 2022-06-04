384cb50afe5ea741a809f243f763835951b2c6b1
Joe Konno <jkonno@seas.upenn.edu>
2022-06-04 14:42:45 -0400

    Get rid of react router stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,4 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom/client'
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from 'react-router-dom'
-
-// import Home from './Home'
-// import Organizers from './Organizers'
-// import Hackers from './Hackers'
-// import Ambassadors from './Ambassadors'
 
 export default function Index() {
   return (
