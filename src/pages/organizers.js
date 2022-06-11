c420c923d42977fc9faafeccef3de50be9afcbd7
Yang Du <41162345+ydu20@users.noreply.github.com>
2022-06-11 12:08:08 -0400

    general structure

--- src/pages/organizers.js
+++ src/pages/organizers.js
@@ -1,10 +1,30 @@
 import * as React from 'react'
+import './organizers.css'
+import ApplicationsIntro from './organizers_components/ApplicationsIntro'
+import ApplicationsProcess from './organizers_components/ApplicationsProcess'
+import ApplicationsCommittees from './organizers_components/ApplicationsCommittees'
+import ApplicationsFAQs from './organizers_components/ApplicationsFAQs'
 
 export default function Organizers() {
   return (
     <>
-      <h1>Get Involved - Organizers</h1>
-      <h2>Edit this page as necessary</h2>
+      <div className="nav-bar-placeholder">
+        Nav Bar Placeholder
+      </div>
+      <div className="applications-header-container">
+        <div className="applications">
+          <h3>Applications are Open!</h3>
+        </div>
+        <div className="pictures">
+          Illustration or exec pic here
+        </div>
+      </div>
+      <div className="applications-body-container">
+        <ApplicationsIntro />
+        <ApplicationsProcess />
+        <ApplicationsCommittees />
+        <ApplicationsFAQs />
+      </div>
     </>
   )
 }
---
+++ src/pages/organizers_components/ApplicationsFAQs.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+export default function ApplicationsFAQs() {
+    return (
+        <div className="applications-faqs">
+            <h3>FAQs</h3>
+        </div>
+    )
+}
