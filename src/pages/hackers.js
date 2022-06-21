6522e107f97aa567583039028bfe1220776698a1
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2022-06-20 21:27:15 -0400

    Added iteration logo (temp), header, fixed sizing of apply box text, still need answer text for questions and footer

--- src/pages/hackers.js
+++ src/pages/hackers.js
@@ -2,32 +2,43 @@ import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import { useSpring, animated } from 'react-spring'
 import chevronImage from '../images/arrow-east.png'
+import iterationLogo from '../images/cloud_square_logo 1.png'
 import './hackers.css'
 
 export default function Hackers() {
   return (
-    <div className="hackers-page">
-      <div className="apply-box">
-        <h1 className="apply-header-text">Apply to PennApps XXIII!</h1>
-        <p>Applications are now open</p>
-        <div>
-          <Link to="/dashboard">
-            <button type="button">Apply</button>
-          </Link>
-          <Link to="/dashboard">
-            <button type="button">Event Site</button>
-          </Link>
-        </div>
+    <div>
+      <div className="page-header">
+        <Header />
       </div>
-      <h1 className="faq-text">FAQ</h1>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
-      <hr className="topHR" />
+      <div className="hackers-page">
+        <div className="apply-logo-rect">
+          <div className="apply-box">
+            <h1 className="apply-header-text">Apply to PennApps XXIII!</h1>
+            <p>Applications are now open</p>
+            <div>
+              <Link to="/dashboard">
+                <button className="apply-button" type="button">APPLY</button>
+              </Link>
+              <Link to="/dashboard">
+                <button className="apply-button" type="button">EVENT SITE</button>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <img className="iteration-logo" src={iterationLogo} alt="Iteration Logo" />
+          </div>
+        </div>
+        <h1 className="faq-text">FAQ</h1>
+        <div><QuestionCard questionHead="Why PennApps?" questionBody="PennApps is the oldest and one of the largest hackathons hosted by college students. Attending PennApps means a valuable experience in software development through developing your project and attending workshops. More importantly, this is a place where hundreds of passionate hackers get together to learn, connect, and inspire each other." /></div>
+        <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
+        <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
+        <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
+        <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
+        <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
+        <div><QuestionCard questionHead="Question" questionBody="Answer" /></div>
+        <hr className="topHR" />
+      </div>
     </div>
   )
 }
@@ -80,5 +91,59 @@ function SlidingText({ isOpen, setOpen, text }) {
 
   return (
     <animated.p className="slidingText" style={contentProps}>{text}</animated.p>
+  )
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <div>
+        <Link to="/">
+          <button className="pennapps-button" type="button">PENNAPPS</button>
+        </Link>
+      </div>
+      <div className="pages-nav">
+        <Link to="/hackers">
+          {
+            window.location.href.endsWith('hackers')
+            && <button className="header-bold-button" type="button">HACKERS</button>
+          }
+          {
+            !window.location.href.endsWith('hackers')
+            && <button className="header-button" type="button">HACKERS</button>
+          }
+        </Link>
+        <Link to="/organizers">
+          {
+            window.location.href.endsWith('organizers')
+            && <button className="header-bold-button" type="button">ORGANIZERS</button>
+          }
+          {
+            !window.location.href.endsWith('organizers')
+            && <button className="header-button" type="button">ORGANIZERS</button>
+          }
+        </Link>
+        <Link to="/ambassadors">
+          {
+            window.location.href.endsWith('ambassadors')
+            && <button className="header-bold-button" type="button">AMBASSADORS</button>
+          }
+          {
+            !window.location.href.endsWith('ambassadors')
+            && <button className="header-button" type="button">AMBASSADORS</button>
+          }
+        </Link>
+        <Link to="/moreinfo">
+          {
+            window.location.href.endsWith('moreinfo')
+            && <button className="header-bold-button" type="button">MORE INFO</button>
+          }
+          {
+            !window.location.href.endsWith('moreinfo')
+            && <button className="header-button" type="button">MORE INFO</button>
+          }
+        </Link>
+      </div>
+    </div>
   )
 }
