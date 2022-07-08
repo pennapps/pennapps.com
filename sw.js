32a73cb871143f0e4c77f7f41d332236e037179d
Joe Konno <53798807+MechaJoe@users.noreply.github.com>
2022-07-08 01:04:43 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-348ef88777c9982be7cf.js"
+    "url": "webpack-runtime-ff1fc9b79ee06bb59106.js"
   },
   {
     "url": "framework-bc9ca1d2e20614b36b25.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4cb2e0100ee9862bdeb2fff6aa1b0b74"
+    "revision": "0b1b576c99172d91193a1e1b39d796cd"
   },
   {
     "url": "polyfill-4b59c96585f810a734f1.js"
