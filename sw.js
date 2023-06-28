dcf5b6c4150dfe61904c6c72d9da93d6d38bdeb6
vcai122 <vincent.delaware@gmail.com>
2023-06-27 22:05:17 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ddb17a474129a80260d5.js"
+    "url": "webpack-runtime-6731182707c3f27643c9.js"
   },
   {
     "url": "framework-1de0fc4b8ce49c5123c2.js"
   },
   {
-    "url": "app-12b322903e1ab9d92be6.js"
+    "url": "app-53d7e2bd345c6403f193.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0197ce42a7d81930fa173ae65567f40e"
+    "revision": "60bdaa4800355ec170ea81ae8e488eb4"
   },
   {
     "url": "polyfill-14b0839a2daa5f76ae01.js"
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-12b322903e1ab9d92be6.js`))) {
+  if (!resources || !(await caches.match(`/app-53d7e2bd345c6403f193.js`))) {
     return await fetch(event.request)
   }
 
