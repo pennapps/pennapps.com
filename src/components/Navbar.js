eae7ee184c4bc7d98ff73b96c5396175f4214038
gumillie222 <121700352+gumillie222@users.noreply.github.com>
2024-04-12 23:54:04 -0400

    fix interest form link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,31 +47,30 @@ function DrawerAppBar({ currPage }) {
     </Box>
   )
 
-
   return (
     <>
       <Box
-  sx={{
-    position: 'fixed',
-    top: 0,
-    width: '100%',
-    height: '30px',
-    zIndex: 1000,
-    bgcolor: '#990000',
-    color: '#dce2e8',
-    textAlign: 'center',
-    padding: '10px',
-  }}
->
-<Typography variant="body1" sx={{ fontFamily: `'futura-pt', 'Futura', 'Trebuchet MS', 'Arial', 'sans-serif'` }}>
-  <b> FILL OUT THIS{' '} </b>
-  <b href="https://docs.google.com/forms/d/e/1FAIpQLSemhXtQOOhTzAsz06QA8t9aU-shJs_ZukuU6rW8Nvo2FoiavQ/viewform" style={{ color: '#7fa3c9', textDecoration: 'underline', fontWeight: 'inherit', fontFamily: `'futura-pt', 'Futura', 'Trebuchet MS', 'Arial', 'sans-serif'` }}>
-    FORM
-  </b>{' '}
-  <b>TO BE THE FIRST TO KNOW WHEN THE APPLICATIONS OPEN FOR 2024! </b>
-</Typography>
+        sx={{
+          position: 'fixed',
+          top: 0,
+          width: '100%',
+          height: '30px',
+          zIndex: 1000,
+          bgcolor: '#990000',
+          color: '#dce2e8',
+          textAlign: 'center',
+          padding: '10px',
+        }}
+      >
+        <Typography variant="body1" sx={{ fontFamily: `'futura-pt', 'Futura', 'Trebuchet MS', 'Arial', 'sans-serif'` }}>
+          <b> FILL OUT THIS{' '} </b>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSemhXtQOOhTzAsz06QA8t9aU-shJs_ZukuU6rW8Nvo2FoiavQ/viewform" style={{ color: '#7fa3c9', textDecoration: 'underline', fontWeight: 'inherit', fontFamily: `'futura-pt', 'Futura', 'Trebuchet MS', 'Arial', 'sans-serif'` }}>
+            FORM
+          </a>{' '}
+          <b>TO BE THE FIRST TO KNOW WHEN THE APPLICATIONS OPEN FOR 2024! </b>
+        </Typography>
 
-</Box>
+      </Box>
 
       <AppBar component="nav" sx={{ top: '45px', bgcolor: 'rgba(255, 255, 255, 0.8)', position: 'relative', zIndex: 1 }} elevation={0}>
         <Toolbar>
